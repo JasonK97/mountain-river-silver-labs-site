9ef87f26d85c2eee54c0d6cd9f37c8554849b9dc
Jason Kent <jasonbkent@gmail.com>
2021-12-01 23:54:40 -0700

    Started on the contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import greenMountains from '../assets/022_15-2.jpg'
+
 const Contact = () => {
   return (
     <div className='contact-page'>
-      This is a contact page
+      <img src={greenMountains} alt='beautiful green tree covered mountains' className='contact-img' />
+      <div className='contact-info'>
+        <h2>Contact Us</h2>
+        <p>
+          Email: mtn.river.silvers@gmail.com<br />
+          Phone: 208-xxx-xxxx
+        </p>
+      </div>
     </div>
   )
 }
